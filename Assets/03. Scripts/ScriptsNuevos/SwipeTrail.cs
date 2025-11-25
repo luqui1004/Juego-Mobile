c4f8f7034eaee2bf1094b0960644198cd0f4a45f
luqui1004 <lucascoceres0@gmail.com>
2025-11-24 22:05:23 -0300

    Se añadió un SwipeTrail

---
+++ Assets/03. Scripts/ScriptsNuevos/SwipeTrail.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class SwipeTrail : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject trailPrefab;
+
+    private GameObject currentTrail;
+
+    void Update()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 pos = cam.ScreenToWorldPoint(new Vector3(
+                touch.position.x,
+                touch.position.y,
+                cam.nearClipPlane + 0.1f
+            ));
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    currentTrail = Instantiate(trailPrefab, pos, Quaternion.identity);
+                    break;
+
+                case TouchPhase.Moved:
+                    if (currentTrail != null)
+                        currentTrail.transform.position = pos;
+                    break;
+
+                case TouchPhase.Ended:
+                    if (currentTrail != null)
+                        Destroy(currentTrail, 0.2f);
+                    break;
+            }
+        }
+
+#endif
+    }
+}
